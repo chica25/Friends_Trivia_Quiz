4740c8ff6978f85270ff7fb223cc660ea3d27cd5
chica25 <patricia@patriciaco.com>
2021-03-08 15:44:34 -0500

    Implement html code

---
+++ friends.js
@@ -0,0 +1,10 @@
+// const allAnswers = ['B', 'B', 'B', 'B'];
+
+// const fom = document.querySelector('.quiz-form');
+
+// form.addEventListerner('submit answer' e => {
+//     e.preventDefault();
+
+//     let score = 0;
+//     const userAnswers = [form]
+// })
