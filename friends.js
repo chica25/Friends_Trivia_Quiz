f2cdf4b6878bd2a7c3a4f399b847737ca31e0daa
chica25 <patricia@patriciaco.com>
2021-03-08 19:04:51 -0500

    Included new functions in friends.js

--- friends.js
+++ friends.js
@@ -1,10 +1,24 @@
-// const allAnswers = ['B', 'B', 'B', 'B'];
 
-// const fom = document.querySelector('.quiz-form');
+const allAnswers = ['B', 'A', 'D', 'B', 'B', 'A', 'D', 'A', 'B', 'C'];
+const form = document.querySelector('.form');
+const result = document.querySelector('.result')
 
-// form.addEventListerner('submit answer' e => {
-//     e.preventDefault();
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-//     let score = 0;
-//     const userAnswers = [form]
-// })
+    let score = 0;
+    
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value]
+    
+    userAnswers.forEach((answer, index) => {
+        if(answer === allAnswers[index]){
+            score += 10;
+        }
+      });
+
+    //  console.log(score);show results on page
+    scrollTo(0 ,0);
+    result.querySelector('span').textContent = `${score}%`;
+    // result.classList.remove('')
+});
+
